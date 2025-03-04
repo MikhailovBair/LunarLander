d31258653393124dce6c350fa606a1231f4bec6f
MikhailovBair <bair1209@gmail.com>
2025-03-04 17:33:55 +0300

    Added greedy agent

--- src/agent/policy_agent.py
+++ src/agent/policy_agent.py
@@ -1,7 +1,7 @@
+import torch
 from agent import Agent
 from config import device
 from policy import Policy
-
 
 class PolicyAgent(Agent):
     def __init__(self, policy: Policy):
@@ -11,5 +11,14 @@ class PolicyAgent(Agent):
         distribution = self.policy(observation)
         action = distribution.sample()
         action_log_probability = distribution.log_prob(action)
-        return action, action_log_probability.unsqueeze(0)
+        return action, action_log_probability.unsqueeze(-1)
 
+
+class GreedyAgent(PolicyAgent):
+    def get_action(self, observation):
+        if observation[6] and observation[7]:
+            return torch.tensor(0), torch.tensor(1)
+        distribution = self.policy(observation)
+        action = distribution.sample()
+        action_log_probability = distribution.log_prob(action)
+        return action, action_log_probability.unsqueeze(-1)
